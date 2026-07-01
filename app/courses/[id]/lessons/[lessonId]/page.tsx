4d04d3ec58c30a3a02d77431f5ca662d1dc94cf9
Mustapha Adamu <mustaphaadamu6773@gmail.com>
2026-07-01 16:28:31 -0700

    Fix lesson redirect URL to courses/{id}/lessons/{lessonId}

--- app/courses/[id]/lessons/[lessonId]/page.tsx
+++ app/courses/[id]/lessons/[lessonId]/page.tsx
@@ -9,7 +9,7 @@ export default function LessonPage({ params }: Props) {
   const { id, lessonId } = use(params);
 
   useEffect(() => {
-    window.location.href = `https://musteydigitalacademy.online/lessons/${id}/${lessonId}`;
+    window.location.href = `https://musteydigitalacademy.online/courses/${id}/lessons/${lessonId}`;
   }, [id, lessonId]);
 
   return <p className="p-10 text-gray-500">Loading lesson...</p>;
